5b771850c5dfec8778c3fe16ab9c427f5271d428
Jake David <jakedgmu13@gmail.com>
2019-06-15 17:46:47 -0400

    created a seeds.sql and relocated all seeds there, created model files for all tables

---
+++ migrations/20190615211300-create-schedule.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('schedules', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      hours: {
+        type: Sequelize.INTEGER
+      },
+      monday: {
+        type: Sequelize.STRING
+      },
+      tuesday: {
+        type: Sequelize.STRING
+      },
+      wednesday: {
+        type: Sequelize.STRING
+      },
+      thursday: {
+        type: Sequelize.STRING
+      },
+      friday: {
+        type: Sequelize.STRING
+      },
+      saturday: {
+        type: Sequelize.STRING
+      },
+      sunday: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('schedules');
+  }
+};
---
+++ migrations/20190615213133-create-payroll-employees.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('payroll_employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      annual_salary: {
+        type: Sequelize.DECIMAL
+      },
+      monthly_salary: {
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('payroll_employees');
+  }
+};
